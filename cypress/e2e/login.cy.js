b55a6e5b14c64a3cd6824ad41b6bcb2511f7667f
Artem Pilipenko <artem.pilipenko97@gmail.com>
2024-11-14 02:38:08 +0300

    login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -14,6 +14,7 @@ it('Верный логин и верный пароль', function () {
         cy.get('#messageHeader').should('be.visible'); 
         cy.get('#exitMessageButton > .exitIcon').should('be.visible'); 
     })
+   
 it('Проверка на приведение к строчным буквам в логине', function () {
         cy.visit('https://login.qa.studio/'); 
         cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)'); 
